3a704c3c69d760d96e7a71a6088a749b028c0459
Mimin <mincong.h@gmail.com>
2015-03-01 22:07:52 +0100

    Create main.py

---
+++ Tutorial-9_Sequences-and-Lists/main.py
@@ -0,0 +1,8 @@
+coloc = ['mincong','pape','charline']
+print(coloc[0]) # mincong
+print(coloc[1]) # pape
+print(coloc[2]) # charline
+
+# Pay attention !
+print(coloc[-1]) # charline
+coloc[0][2] # 'mincong'[2] # 'n'
