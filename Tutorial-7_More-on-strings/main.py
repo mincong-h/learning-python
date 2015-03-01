7853ba560a7db86335516b145cb75ec3f5a538a4
Mimin <mincong.h@gmail.com>
2015-03-01 21:37:14 +0100

    Create main.py

---
+++ Tutorial-7_More-on-strings/main.py
@@ -0,0 +1,16 @@
+"hey now"
+print "hey now"
+
+# 1.1
+num = 18
+num + 16
+print("Bucky id" + num) # ERROR!! - connot concatenate 'str' and 'int' objects
+
+# 1.2
+print("Bucky is " + str(num)) # OK
+
+# 1.3
+print("My mom is " + `num`) # OK in the video but connot work in my Mac
+
+# 2
+repr() # ?
