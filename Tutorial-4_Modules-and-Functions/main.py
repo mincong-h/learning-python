8c6301289b7d64338385ce30c2a8b8f5909e1c87
Mimin <mincong.h@gmail.com>
2015-03-01 17:07:51 +0100

    Create main.py

---
+++ Tutorial-4_Modules-and-Functions/main.py
@@ -0,0 +1,13 @@
+5 ** 4 # 625
+pow(5, 4) # 625
+abs(-5) # 5
+#floor(18.7) # ERROR!!!
+
+#import module
+import math
+math.floor(18.7) # 18
+math.sqrt(81) # 9
+bucky = math.sqrt
+bucky(9) # 3.0
+bucky = math.floor
+bucky (19.8) # 19
