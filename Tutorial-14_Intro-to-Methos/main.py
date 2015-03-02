3f22f125d41306adab5689f28a95e60d1da4ec04
Mimin <mincong.h@gmail.com>
2015-03-02 23:46:55 +0100

    Create main.py

---
+++ Tutorial-14_Intro-to-Methos/main.py
@@ -0,0 +1,11 @@
+# .appren()
+face=[21, 18, 30] # [21, 18, 30]
+face.append(45) # [21, 18, 30, 45]
+
+# .count()
+apples.count('apples') # 2
+
+# .extend()
+one = [1, 2, 3]
+two = [4, 5, 6]
+one.extend(two) #[1, 2, 3, 4, 5, 6]
