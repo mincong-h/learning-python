d46d47f5a4792ed6c5b619522604ae85caff9826
Mimin <mincong.h@gmail.com>
2015-03-03 09:09:54 +0100

    Update main.py

--- Tutorial-21_Function-and-return/main.py
+++ Tutorial-21_Function-and-return/main.py
@@ -1,6 +1,20 @@
-def allowed_dating_age(my_age):
+# 1
+def allowed_dating_age(my_age) :
   girls_age = my_age / 2 + 7
   return girls_age
-
+# 1.1 example
 mincongs_limit = allowed_dating_age()
 print("i can date firls", mincongs_limit, "or older")
+
+
+# 2
+def get_gender(sex='unknown') :
+  if sex is 'm' :
+    sex = "Male"
+  elif sex is 'f' :
+    sex = "Female"
+  print(sex)
+# 2.1 example
+get_gender('m') # 'Male'
+get_gender('f') # 'Female'
+get_gender() # 'unknown'
