ceecfed793521014d833817a95bc58bf875d1a81
Mimin <mincong.h@gmail.com>
2015-03-03 08:33:38 +0100

    Create main.py

---
+++ Tutorial-17_for/main.py
@@ -0,0 +1,6 @@
+foods = ['bacon', 'tuna', 'ham', 'snausages', 'beef']
+for f in foods[1:] : # starts at index 1 'tuna'
+  print(f)
+  print(len(f))
+  if f is 'tuna' :
+    print("I don't know what it is")
