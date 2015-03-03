9876e98d0d913780d620215ad50a3e7c21e77f1c
Mimin <mincong.h@gmail.com>
2015-03-03 08:03:03 +0100

    Update main.py

--- Tutorial-15_More-Methods/main.py
+++ Tutorial-15_More-Methods/main.py
@@ -1,1 +1,9 @@
+say = ['hey', 'now', 'brown', 'cow']
+say.index('brown') # 2
+say.insert(2, 'hoss') # ['hey', 'now', 'hoss', 'brown', 'cow']
 
+# .pop() & .remove()
+say.pop(1) # 'now'
+say # ['hey', 'hoss', 'brown', 'cow']
+say.remove('brown') # NULL
+say # ['hey', 'hoss', 'cow']
