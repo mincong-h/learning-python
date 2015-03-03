f853e5377025e22399dcfef4fc0facbb0700c9f1
Mimin <mincong.h@gmail.com>
2015-03-03 09:36:57 +0100

    Create main.py

---
+++ Tutorial-25_My-trip-to-Walmart/main.py
@@ -0,0 +1,7 @@
+coloc = {'mincong', 'mincong', 'pape', 'charline'}
+print(coloc) # {'mincong', 'pape', 'charline'}  # /!\ 'mincong' printed only once
+
+if 'mincong' in coloc :
+  print("He's here.")
+else : 
+  print("He is not here.")
