a2bfb7d937a37830898922ea2d46099c075f8f50
mincong-h <mincong.h@gmail.com>
2015-10-25 16:26:53 +0100

    add week4 ex

---
+++ coursera/w4/data_pd.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+4.2 Python 高级数据处理与可视化
+
+"""
+
+import pandas as pd
+closeMeansKO.plot()
+plt.title('Stock Statistics of Coca-Cola')
+plt.xlabel('Month')
+plt.ylabel('Average Close Price')
