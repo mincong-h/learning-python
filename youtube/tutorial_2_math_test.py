0c50eab66d90d966085e15300244f78a237204c2
Mincong HUANG <mincong.h@gmail.com>
2016-11-27 18:54:32 +0100

    refactor youtube tutorial 1 - 2

---
+++ youtube/tutorial_2_math_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_math(self):
+    self.assertEqual(2 + 3, 5)
+    self.assertEqual(2 - 3, -1)
+    self.assertEqual(2 * 3, 6)
+    self.assertEqual(2 / 3, 0)
+    self.assertEqual(3 / 2, 1)
+    self.assertEqual(2 % 3, 2)
+    self.assertEqual(2 ** 3, 8)
+
+if __name__ == '__main__':
+  unittest.main()
