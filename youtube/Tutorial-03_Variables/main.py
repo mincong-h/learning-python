5e15cee64e4732a80eb98496937cd6785f779b13
Mincong HUANG <mincong.h@gmail.com>
2016-11-28 00:04:46 +0100

    refactor youtube tutorial 3

--- youtube/Tutorial-03_Variables/main.py
+++
@@ -1,7 +0,0 @@
-x = 18 # 18
-x + 15 # 33
-x ** 3 # 5852
-y = 54 # 54
-x + y # 72
-g = input("Enter number here: ") # string
-g = int(input("Enter number here: ")) # integer
---
+++ youtube/tutorial_3_variables_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from contextlib import contextmanager
+import tutorial_3_variables as tutorial
+import unittest
+
+
+class TestVariables(unittest.TestCase):
+
+  def test_variables(self):
+    x = 18
+    y = 54
+    self.assertEqual(x + 15, 33)
+    self.assertEqual(x ** 3, 5832)
+    self.assertEqual(x + y, 72)
+
+
+if __name__ == '__main__':
+  unittest.main()
