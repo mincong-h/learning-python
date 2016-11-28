1900b72b8df412c3f456ef6b0f98213a6d9af8cd
Mincong HUANG <mincong.h@gmail.com>
2016-11-28 15:18:05 +0100

    refactor youtube tutorial 7

---
+++ youtube/tutorial_7_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Person:
+
+  def __init__(self, name):
+    self.name = name
+
+  def __repr__(self):
+    return "Hi, my name is " + self.name + "!"
+
+
+def main():
+  person = Person('Tom')
+  print person
+
+if __name__ == '__main__':
+  main()
---
+++ youtube/tutorial_7_string_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tutorial_7_string as tutorial
+import unittest
+
+
+class TestStringOperations(unittest.TestCase):
+
+  def test_concat_str_int(self):
+    num = 18
+    # TypeError: cannot concatenate 'str' and 'int' objects
+    with self.assertRaises(TypeError):
+      'num ' + num
+
+  def test_concat_str_str(self):
+    num = 18
+    self.assertEqual('num ' + str(num), 'num 18')
+
+  def test_repr(self):
+    person = tutorial.Person('Tom')
+    self.assertEqual(person.name, 'Tom')
+    self.assertEqual(repr(person), "Hi, my name is Tom!")
+    # If __repr__ is overridden, __str__ is overridden too.
+    # But not vice versa.
+    self.assertEqual(str(person), "Hi, my name is Tom!")
+
+
+if __name__ == '__main__':
+  unittest.main()
