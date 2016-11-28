8504d06acc26ba50984b802dc08582b34238b257
Mincong HUANG <mincong.h@gmail.com>
2016-11-28 16:58:25 +0100

    refactor youtube tutorial 9

--- youtube/Tutorial-09_Sequences-and-Lists/main.py
+++
@@ -1,8 +0,0 @@
-coloc = ['mincong','pape','charline']
-print(coloc[0]) # mincong
-print(coloc[1]) # pape
-print(coloc[2]) # charline
-
-# Pay attention !
-print(coloc[-1]) # charline
-coloc[0][2] # 'mincong'[2] # 'n'
