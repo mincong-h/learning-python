1329d3292322eaa0e71641356cabce9baf2b65cf
Mincong HUANG <mincong.h@gmail.com>
2016-11-29 16:10:53 +0100

    refactor youtube tutorial 25

--- youtube/Tutorial-25_My-trip-to-Walmart/main.py
+++
@@ -1,7 +0,0 @@
-coloc = {'mincong', 'mincong', 'pape', 'charline'}
-print(coloc) # {'mincong', 'pape', 'charline'}  # /!\ 'mincong' printed only once
-
-if 'mincong' in coloc :
-  print("He's here.")
-else : 
-  print("He is not here.")
