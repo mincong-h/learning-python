0b9bebb76b7e815835704895492d8545612f30d3
Mincong HUANG <mincong.h@gmail.com>
2020-07-02 22:38:33 +0200

    Implement the test

--- test/stdio/hello_test.py
+++ test/stdio/hello_test.py
@@ -1,5 +1,14 @@
+import io
+import sys
 import unittest
+from unittest.mock import patch
+
+import src.stdio.hello as script
+
 
 class TestHello(unittest.TestCase):
-    def test_main(self):
-        self.assertTrue(True)
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_main(self, mock_stdout):
+        sys.argv[1:] = ["Python"]
+        script.main()
+        self.assertEqual("Hello, Python\n", mock_stdout.getvalue())
