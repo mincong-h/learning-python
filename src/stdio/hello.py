fefb218f07598757d37f3c1769b34e544598ed8a
Mincong HUANG <mincong.h@gmail.com>
2020-07-02 22:05:47 +0200

    Standard I/O

---
+++ src/stdio/hello.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def main():
+  name = sys.argv[1]
+  print(f"Hello, {name}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/stdio/hello_test.py
@@ -0,0 +1,5 @@
+import unittest
+
+class TestHello(unittest.TestCase):
+    def test_main(self):
+        self.assertTrue(True)
