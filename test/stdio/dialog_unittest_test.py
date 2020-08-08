4fa17628f7a87c1b9f26318447ad66e58f6ef7b5
Mincong HUANG <mincong.h@gmail.com>
2020-08-08 08:47:39 +0200

    Rename methods

--- test/stdio/dialog_unittest_test.py
+++ test/stdio/dialog_unittest_test.py
@@ -8,7 +8,7 @@ import src.stdio.dialog as script
 class TestHello(unittest.TestCase):
     @patch("builtins.input", side_effect=["y"])
     @patch("sys.stdout", new_callable=io.StringIO)
-    def test_main(self, mock_stdout, mock_stdin):
+    def test_main_yes(self, mock_stdout, mock_stdin):
         script.main()
         self.assertEqual(
             "Welcome to Mincong's demo\nThanks for confirmation\n",
@@ -17,7 +17,7 @@ class TestHello(unittest.TestCase):
 
     @patch("builtins.input", side_effect=["n"])
     @patch("sys.stdout", new_callable=io.StringIO)
-    def test_main(self, mock_stdout, mock_stdin):
+    def test_main_no(self, mock_stdout, mock_stdin):
         script.main()
         self.assertEqual(
             "Welcome to Mincong's demo\nSorry to see you go\n", mock_stdout.getvalue()
