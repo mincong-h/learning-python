80c405b2a3546780a95958ff776f15b9f3fa6d58
Mincong HUANG <mincong.h@gmail.com>
2020-12-19 19:16:21 +0100

    Add new test (demo)

---
+++ test/http/es_pytest_test.py
@@ -0,0 +1,16 @@
+import responses
+import requests
+
+
+@responses.activate
+def test_simple():
+    responses.add(responses.GET, 'http://twitter.com/api/1/foobar',
+                  json={'error': 'not found'}, status=404)
+
+    resp = requests.get('http://twitter.com/api/1/foobar')
+
+    assert resp.json() == {"error": "not found"}
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://twitter.com/api/1/foobar'
+    assert responses.calls[0].response.text == '{"error": "not found"}'
