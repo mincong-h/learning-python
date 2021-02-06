ccbe037975d4b5be85156c323dca3b9992d25b5c
Mincong HUANG <mincong.h@gmail.com>
2021-02-06 18:40:51 +0100

    Add object in rendering

--- src/jinja2/templating.py
+++ src/jinja2/templating.py
@@ -1,6 +1,13 @@
 import os
+from typing import NamedTuple
 
 from jinja2 import Template, FileSystemLoader, Environment
+
+
+class DockerInfo(NamedTuple):
+    image_id: str
+    image_name: str
+    image_version: str
 
 
 def say_hello(name: str) -> str:
@@ -8,8 +15,8 @@ def say_hello(name: str) -> str:
     return template.render(name=name)
 
 
-def render_app(version: str) -> str:
+def render_app(version: str, docker: DockerInfo) -> str:
     template_dir = os.path.join(os.path.dirname(__file__), "templates")
     env = Environment(loader=FileSystemLoader(template_dir))
     template = env.get_template("app.j2")
-    return template.render(version=version)
+    return template.render(version=version, docker=docker)
--- test/jinja2/templating_test.py
+++ test/jinja2/templating_test.py
@@ -7,7 +7,17 @@ def test_say_hello():
 
 
 def test_render_app():
-    yml = render_app(version="1.0.0")
-    assert yml == """\
+    yml = render_app(
+        version="1.0.0",
+        docker=DockerInfo(image_id="12345", image_name="python", image_version="3.8.7"),
+    )
+    assert (
+        yml
+        == """\
 name: my-app
-version: 1.0.0"""
+version: 1.0.0
+docker:
+  image_name: python
+  image_id: 12345
+  image_version: 3.8.7"""
+    )
