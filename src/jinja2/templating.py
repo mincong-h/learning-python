623b85ffb1438e470c529d46b92e472419882563
Mincong HUANG <mincong.h@gmail.com>
2021-02-06 18:21:04 +0100

    Demo: render_app()

--- src/jinja2/templating.py
+++ src/jinja2/templating.py
@@ -1,6 +1,15 @@
-from jinja2 import Template
+import os
+
+from jinja2 import Template, FileSystemLoader, Environment
 
 
 def say_hello(name: str) -> str:
     template = Template("Hello, {{ name }}!")
     return template.render(name=name)
+
+
+def render_app(version: str) -> str:
+    template_dir = os.path.join(os.path.dirname(__file__), "templates")
+    env = Environment(loader=FileSystemLoader(template_dir))
+    template = env.get_template("app.j2")
+    return template.render(version=version)
--- test/jinja2/templating_test.py
+++ test/jinja2/templating_test.py
@@ -4,3 +4,10 @@ from src.jinja2.templating import *
 def test_say_hello():
     message = say_hello("Python")
     assert message == "Hello, Python!"
+
+
+def test_render_app():
+    yml = render_app(version="1.0.0")
+    assert yml == """\
+name: my-app
+version: 1.0.0"""
