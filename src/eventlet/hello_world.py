8dcc07ad0ef8e3e12705a9639d430c4d88502289
Mincong HUANG <mincong.h@gmail.com>
2021-05-17 21:15:58 +0200

    Demo1 ok

--- src/eventlet/hello_world.py
+++ src/eventlet/hello_world.py
@@ -7,12 +7,19 @@ def say_hello(name: str):
     return f"Hello, {name}!"
 
 
-def main():
-    print(f"-- start: {datetime.now().time()}")
+def demo1():
+    """
+    output:
+
+    -- start: 21:14:46.666814 -----
+    Hello, world!
+    ---- end: 21:14:46.667007 -----
+    """
+    print(f"-- start: {datetime.now().time()} -----")
     sentence = eventlet.timeout.with_timeout(30, say_hello, "world")
     print(sentence)
-    print(f"---- end: {datetime.now().time()}")
+    print(f"---- end: {datetime.now().time()} -----")
 
 
 if __name__ == "__main__":
-    main()
+    demo1()
