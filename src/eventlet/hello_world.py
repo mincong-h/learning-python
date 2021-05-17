bed9fbafdba69fcd6a8a1b7079d06232658d6ee8
Mincong HUANG <mincong.h@gmail.com>
2021-05-17 21:34:55 +0200

    Add more demos

--- src/eventlet/hello_world.py
+++ src/eventlet/hello_world.py
@@ -12,14 +12,85 @@ def demo1():
     output:
 
     -- start: 21:14:46.666814 -----
-    Hello, world!
+    Hello, demo1!
     ---- end: 21:14:46.667007 -----
     """
     print(f"-- start: {datetime.now().time()} -----")
-    sentence = eventlet.timeout.with_timeout(30, say_hello, "world")
+    sentence = eventlet.timeout.with_timeout(5, say_hello, "demo1")
     print(sentence)
+    print(f"---- end: {datetime.now().time()} -----")
+
+
+def demo2():
+    """
+    output:
+
+    -- start: 21:19:42.640061 -----
+    Hello, demo2!
+    ---- end: 21:19:42.640103 -----
+    """
+    print(f"-- start: {datetime.now().time()} -----")
+    with eventlet.timeout.Timeout(5):
+        sentence = say_hello("demo2")
+    print(sentence)
+    print(f"---- end: {datetime.now().time()} -----")
+
+
+def demo3():
+    """
+    output:
+
+    -- start: 21:33:04.221446 -----
+    0
+    1
+    2
+    3
+    4
+    ---- end: 21:33:09.225682 -----
+    """
+    print(f"-- start: {datetime.now().time()} -----")
+    with eventlet.timeout.Timeout(5, False):
+        i = 0
+        while i < 10:
+            print(i)
+            eventlet.sleep(1)
+            i += 1
+    print(f"---- end: {datetime.now().time()} -----")
+
+
+def demo4():
+    """
+    output:
+
+    -- start: 21:33:09.225741 -----
+    0
+    1
+    2
+    3
+    4
+    Traceback (most recent call last):
+      File "src/eventlet/hello_world.py", line 77, in <module>
+        demo4()
+      File "src/eventlet/hello_world.py", line 68, in demo4
+        eventlet.sleep(1)
+      File "/Users/minconghuang/.local/share/virtualenvs/learning-python-8MTqpB0U/lib/python3.7/site-packages/eventlet/greenthread.py", line 36, in sleep
+        hub.switch()
+      File "/Users/minconghuang/.local/share/virtualenvs/learning-python-8MTqpB0U/lib/python3.7/site-packages/eventlet/hubs/hub.py", line 313, in switch
+        return self.greenlet.switch()
+    eventlet.timeout.Timeout: 5 seconds
+    """
+    print(f"-- start: {datetime.now().time()} -----")
+    with eventlet.timeout.Timeout(5):
+        i = 0
+        while i < 10:
+            print(i)
+            eventlet.sleep(1)
+            i += 1
     print(f"---- end: {datetime.now().time()} -----")
 
 
 if __name__ == "__main__":
     demo1()
+    demo2()
+    demo3()
+    demo4()
